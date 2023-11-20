6a2015883e82d5b0af4ca7e046ce8dfc3bc8bdef
Kamil <kkowalczyk848@gmail.com>
2023-11-20 16:16:03 +0100

    Adjusted gitignore

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from support import (
 
 # Read file names from directory
 list_dataset = tf.data.Dataset.list_files(os.getcwd() + r'/dataset_augmented/*/*', shuffle=True)
-ds_validation = tf.data.Dataset.list_files(os.getcwd() + r'/dataset/*/*', shuffle=True)
+ds_validation = tf.data.Dataset.list_files(os.getcwd() + r'/dataset_validation/*/*', shuffle=True)
 
 # Create labeled dataset from list of files
 labeled_dataset = list_dataset.map(extract_image_and_label)
@@ -80,7 +80,6 @@ plt.show()
 test_loss, test_acc = signs_classification_model.evaluate(ds_test)
 print(f'test loss: {test_loss}, test_acc: {test_acc}')
 
-# TODO: predictions with confusion matrix and clean up the rest of the code
 # Extract images and labels from the validation dataset
 validation_images, validation_labels = zip(*[(image, label) for image, label in ds_validation.map(extract_image_and_label)])
 
